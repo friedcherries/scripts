7934b73e7756fbf88ce4ec949eec5f204f73bfc1
Michael D. Culey <michael@culey.dev>
2022-09-08 12:34:03 -0500

    Swapped out location IP variable name

--- bash/dns/digital-ocean-auto-dns.sh
+++ bash/dns/digital-ocean-auto-dns.sh
@@ -3,7 +3,7 @@
 TOKEN_FILE=${1}
 DOMAINS=${2}
 
-GOOGLE_IP=$(curl -s http://whatismyip.akamai.com/)
+HOME_IP=$(curl -s http://whatismyip.akamai.com/)
 
 function get_domain_id() {
     DOMAIN=${1}
@@ -22,15 +22,15 @@ function update_domain_ip() {
          -K ${TOKEN_FILE} \
          -H "Content-type: application/json" \
          -H "Cache-control: no-cache" \
-         -d "{\"data\": \"${GOOGLE_IP}\"}"
+         -d "{\"data\": \"${HOME_IP}\"}"
     echo
 }
 
 for D in ${DOMAINS}; do
     DOMAIN_IP=$(dig +short ${D})
-    if [ "${DOMAIN_IP}" != "${GOOGLE_IP}" ]; then
+    if [ "${DOMAIN_IP}" != "${HOME_IP}" ]; then
         DID=$(get_domain_id $D)
-        echo "Updating ${D} IP to ${GOOGLE_IP}"
+        echo "Updating ${D} IP to ${HOME_IP}"
         RESPONSE=$(update_domain_ip ${D} ${DID})
         echo ${RESPONSE}
     fi
